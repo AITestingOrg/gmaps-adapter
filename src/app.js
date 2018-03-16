ac91f2d127d20edf54f5d2dcfccca256a4639fce
Gabriel Perez Clark <gaboperezclark@gmail.com>
2018-03-16 18:22:44 -0400

    Removed console logs and cleaned some code

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
 var restify = require('restify');
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
-
 var app = restify.createServer();
-const restifyBodyParser = require('restify').plugins.bodyParser;
-app.use(restifyBodyParser());
+app.use(restify.plugins.bodyParser());
 
 var routes = require('./routes/gmaps-directions.js');
 routes(app);
 
-app.listen(8080, function() {
+app.listen(8080, () => {
   console.log('%s listening at %s', app.name, app.url);
 });
