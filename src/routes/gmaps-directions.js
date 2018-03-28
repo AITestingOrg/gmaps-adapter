4f9393495914fac43211e094124640db54ef9349
Ivetted <ivette_duran@ultimatesoftware.com>
2018-03-28 08:58:43 -0400

    contract test implementation

--- src/routes/gmaps-directions.js
+++ src/routes/gmaps-directions.js
@@ -1,4 +1,21 @@
-const directionMapping = require('./direction-mapping.js');
+const parser = require('./../utils/parser.js');
+const googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyBrbHj2dKR_VIWZDRGqdMLaq99YP-yHwxY'
+  });
+
+const directionsMapping = (req, res) => {
+  googleMapsClient.directions(req.body, 
+    (err, response) => {
+      if(!err) {
+        const parsed = parser.responseParser(response);
+        res.send(parsed);
+      } else {
+        const parsedError = parser.errorParser(err);
+        res.send(400, parsedError);
+      }
+    }
+  );
+}
 
 module.exports = (app) => {
   app.post('/directions', directionsMapping);
